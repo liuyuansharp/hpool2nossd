5cf82d738c09e737457969386956201573f0674e
liuyuansharp <liuyuansharp@foxmail.com>
2023-01-11 08:29:54 +0800

    fix

--- hpool2nossd.py
+++ hpool2nossd.py
@@ -403,9 +403,9 @@ class hpool2nossd():
         if len(self.plotting_drives) == 0 and len(self.finalizing_drives) == 0:
             if not self.is_all_drives_plots_empty():
                 #重启nossd，更新nossd脚本
-                if self.set_hpool_service("stop"):
+                if self.set_nossd_service("stop"):
                     self.update_nossd_start_sh()
-                    self.set_hpool_service("start")
+                    self.set_nossd_service("start")
             else:
                 print("done")
                 return
