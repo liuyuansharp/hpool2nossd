9c49b7bbdb5a363bb903be1cfeae946946b38433
liuyuansharp <liuyuansharp@foxmail.com>
2023-01-12 22:57:46 +0800

    fix

--- hpool2nossd.py
+++ hpool2nossd.py
@@ -359,17 +359,17 @@ class hpool2nossd():
     def print_drive_info(self, status, d: DriveInfo) -> None:
         print("\nstatus: {}, {:.2f}%".format(status,(self.all_space - self.all_plots*101.3)*100/self.all_space))
 
-        print("drive:  {}".format(d.drive_path))
+        print("drive: {}".format(d.drive_path))
         print(
-            "space:     [total/used/free] : [{}/{}/{}]".format(d.total_gb, d.used_gb, d.free_gb))
+            "space: [total/used/free] : [{}/{}/{}]".format(d.total_gb, d.used_gb, d.free_gb))
         print(
-            "info:      [plots/fpts/spts] : [{}/{}/{}]".format(d.plots_n,d.fpts_n, d.spts_n))
+            "info: [plots/fpts/spts] : [{}/{}/{}]".format(d.plots_n,d.fpts_n, d.spts_n))
         
         all = len(self.all_dirves)
         completed = len(self.readonly_drives)
         uncompleted = len(self.all_dirves) - len(self.readonly_drives)
-        print("space_all: [all/comp/uncomp] : [{}/{}/{}]".format(all,completed,uncompleted))
-        print("info_all : [plots/fpts/spts] : [{}/{}/{}]".format(self.all_plots,self.all_fpts,self.all_spts))
+        print("drive_all: [all/comp/uncomp] : [{}/{}/{}]".format(all,completed,uncompleted))
+        print("info_all: [plots/fpts/spts] : [{}/{}/{}]".format(self.all_plots,self.all_fpts,self.all_spts))
 
         sys.stdout.flush()
 
